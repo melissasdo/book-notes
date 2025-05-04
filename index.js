3f07076a019639b8132f0c30f4b01f3007762850
melissasdo <meljssaoliveira@gmail.com>
2025-05-04 19:13:08 -0300

    Add comments throughout the whole code and resize both the logo and the favicon

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
-import { time } from "console";
-import { isNull } from "util";
+import express from "express"; // Import express to use it in the app
+import ejs from "ejs"; // Import ejs to render HTML templates
+import bodyParser from "body-parser"; // Import body-parser to parse incoming request
+import pg from "pg"; // Import pg to interact with PostgreSQL database
 
-const app = express();
-const port = 3000;
+const app = express(); // Create an instance of express
+const port = 3000; // Define the port on which the server will listen
 
 const db = new pg.Client({
+  // Create a new PostgreSQL client
+  // Connection configuration, change to your own database credentials
   user: "postgres",
   host: "localhost",
   database: "book_notes",
   password: "0509",
   port: 5432,
 });
-db.connect();
+db.connect(); // Connect to the PostgreSQL database
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); // Use body-parser middleware to parse URL-encoded data
+app.use(express.static("public")); // Serve static files from the "public" directory so they can be accessed in the browser
 
+// This function takes a number and returns it as a string with leading zero if it's less than 10, used for time formatting
 function formatTime(number) {
   return number < 10 ? "0" + number : number;
 }
 
+// This returns a greeting based on the current time of day, great way to personalise the user experience
 function timeOfDay() {
   const now = new Date();
   const hours = formatTime(now.getHours());
@@ -33,6 +36,7 @@ function timeOfDay() {
   return "night 🌙";
 }
 
+// Returns the current date in YYYY-MM-DD format, useful for displaying the date when a book was read
 function currentDay() {
   const year = String(formatTime(new Date().getFullYear()));
   const month = String(formatTime(new Date().getMonth() + 1));
@@ -43,11 +47,12 @@ function currentDay() {
 
 // home page = read
 app.get("/", async (req, res) => {
-  const greeting = timeOfDay();
-  const day = currentDay();
+  const greeting = timeOfDay(); // Get the current time of day for greeting
+  const day = currentDay(); // Get the current date
 
-  const result = await db.query("SELECT * FROM books ORDER BY date_read ASC");
+  const result = await db.query("SELECT * FROM books ORDER BY date_read ASC"); // Query the database to get all books ordered by the date read
 
+  // Render the index.ejs template with the list of books and, current date and greeting
   res.render("index.ejs", {
     timeOfDay: greeting,
     listOfBooks: result.rows,
@@ -57,6 +62,7 @@ app.get("/", async (req, res) => {
 
 // create
 app.post("/add", async (req, res) => {
+  // This is the data that will be inserted into the database according to the form in the index.ejs file
   const title = req.body.title;
   const rating = req.body.rating;
   const notes = req.body.notes;
@@ -64,12 +70,14 @@ app.post("/add", async (req, res) => {
   const date_read = req.body.date_read;
 
   try {
+    // Try to insert the new entry into the database. All the fields are required but the 'notes', so if any of them is empty, it will throw an error.
     await db.query(
       "INSERT INTO books (title, rating, notes, author, date_read) VALUES ($1, $2, $3, $4, $5)",
       [title, rating, notes, author, date_read]
     );
     res.redirect("/");
   } catch (err) {
+    // If there is an error, log it to the console and send a 500 status code with an error message
     console.log(err);
     res.status(500).send("Error adding item");
     return;
@@ -78,6 +86,7 @@ app.post("/add", async (req, res) => {
 
 // update
 app.post("/edit", async (req, res) => {
+  // This is the data that will be updated in the database according to the form in the index.ejs file. The id is the primary key of the book that will be updated, and the rest are the fields that will be updated
   const id = req.body.updatedBookId;
   const fields = {
     title: req.body.updatedBookTitle,
@@ -88,7 +97,7 @@ app.post("/edit", async (req, res) => {
   };
 
   try {
-    // Filter only fields that are not null/undefined
+    // This filters only fields that are not null/undefined
     const keysToUpdate = Object.keys(fields).filter(
       (key) => fields[key] !== undefined && fields[key] !== ""
     );
@@ -118,6 +127,7 @@ app.post("/edit", async (req, res) => {
 
 // delete
 app.post("/delete", async (req, res) => {
+  // More straightforward, this is the id of the book that will be deleted from the database (primary key)
   const id = req.body.deleteBookId;
 
   try {
